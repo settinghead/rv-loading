9fd184280c734ea6dd80da7d579dfe214d49cbbb
Xiyang Chen <settinghead@gmail.com>
2014-09-12 19:26:33 -0400

    Use widget-tester for unit tests
    Upgrade angular to 1.2.24

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,13 @@
 
 var gulp = require("gulp"),
     // Include Our Plugins
-    karma = require("gulp-karma"),
     jshint = require("gulp-jshint"),
-    watch = require("gulp-watch"),
-    connect = require("gulp-connect"),
-    httpServer,
-
+    factory = require("widget-tester").gulpTaskFactory,
     //Test files
     testFiles = [
       "components/jQuery/dist/jquery.js",
       "components/q/q.js",
       "components/angular/angular.js",
-      "components/angular-route/angular-route.js",
       "components/angular-mocks/angular-mocks.js",
       "components/angular-spinner/angular-spinner.js",
       "components/spin.js/spin.js",
@@ -47,40 +42,15 @@ gulp.task("lint", function() {
     });
 });
 
-gulp.task("watch", function() {
-    return gulp.watch(filesToLint, ["lint"]);
-});
 
-gulp.task("test", function() {
-  // Be sure to return the stream
-  return gulp.src(testFiles).pipe(
-    watch(function(files) {
-      return files.pipe(karma({
-        configFile: "karma.conf.js",
-        action: "start"
-      }))
-      .on("error", function(err) {
-        // Make sure failed tests cause gulp to exit non-zero
-        throw err;
-      });  
-    }));
-});
-
-gulp.task("server", function() {
-  httpServer = connect.server({
-    root: "./",
-    port: 8000,
-    livereload: true
-  });
-  return httpServer;
-});
+gulp.task("test:unit", ["lint"], factory.testUnitAngular({testFiles: testFiles}));
+gulp.task("test", ["test:unit"]);
 
 gulp.task("default", [], function () {
   console.log("\n***********************");
   console.log("* Tell me what to do: *");
   console.log("***********************");
   console.log("* gulp lint           *");
-  console.log("* gulp watch          *");
   console.log("* gulp test           *");
   console.log("***********************\n");
   return true;
